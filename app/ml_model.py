81780e2d729c6756436c0dbaa72896c607b8fa75
John Mwangi <mail.mwangi@gmail.com>
2025-06-24 21:11:17 +0000

    Add functionality to save to db

--- app/ml_model.py
+++ app/ml_model.py
@@ -5,11 +5,18 @@ from uuid import uuid4
 from time import sleep
 from kafka import KafkaProducer
 import json
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine, Table, MetaData, insert
+
+env_path = "/home/john/mariadb-iceberg-pipeline/dockerfiles/scripts/.env"
+load_dotenv(env_path)
 
 PREDICTIONS_TOPIC = "prediction_details"
 KAFKA_SERVER = "localhost:29092"
+ALERTS_EMAIL = os.environ.get("ALERTS_EMAIL")
 
-def main():
+def send_to_topic():
     producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
     for i in range(10):
@@ -18,6 +25,7 @@ def main():
                 "prediction_id": prediction_id,
                 "customer_id": i + 1,
                 "credit_score": random.randint(0,1000),
+                "email": ALERTS_EMAIL
             }
 
         producer.send(
@@ -27,5 +35,36 @@ def main():
         
         print(f"Successfully sent {prediction_id=} to topic: '{PREDICTIONS_TOPIC}'")
         sleep(2)
+        
+def send_to_db():
+    user = os.environ.get("MARIADB_ROOT_USERNAME")
+    password = os.environ.get("MARIADB_DEMO_PASSWORD")
+    host = os.environ.get("MARIADB_HOST")
+    port = os.environ.get("MARIADB_PORT")
+    db = os.environ.get("ML_DATABASE")
+    dialect = os.environ.get("DATABASE_DIALECT")
+    connector = os.environ.get("DATABASE_CONNECTOR")
+    table_name = os.environ.get("ML_TABLE")
+    
+    db_url = f"{dialect}+{connector}://{user}:{password}@{host}:{port}/{db}"
+    engine = create_engine(db_url)
+    table = Table(table_name, MetaData(), autoload_with=engine)
+    
+    for i in range(10):
+        prediction_id = uuid4().hex
+        prediction = {
+                "prediction_id": prediction_id,
+                "customer_id": i + 1,
+                "credit_score": random.randint(0,1000),
+                "email": ALERTS_EMAIL
+            }
+        
+        stmt = insert(table=table).values(**prediction)
+        with engine.begin() as conn:
+            conn.execute(stmt)
+        
+        print(f"Successfully saved {prediction_id=} to {table_name=}")
+        sleep(2)
+
 if __name__ == "__main__":
-    main()
+    send_to_db()
