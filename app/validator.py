3cff79e1b0fd50f634cbda79fdf16dea2a412118
John Mwangi <mail.mwangi@gmail.com>
2025-07-03 12:02:40 +0000

    Update logic to read from a Debezium topic
    Closes: #67

--- app/validator.py
+++ app/validator.py
@@ -32,7 +32,27 @@ def read_from_simple_topic():
 
 def read_from_debezium_topic():
     """Consumes broker messages that are in Debezium format"""
-    pass
+    PREDICTIONS_TOPIC = "users.machine_learning.predictions"
+    consumer = KafkaConsumer(PREDICTIONS_TOPIC, bootstrap_servers=KAFKA_SERVER)
+    producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+    print(f"Validator is listening to topic: {PREDICTIONS_TOPIC}")
+
+    counter = 0
+    while True:
+        for msg in consumer:
+            event = json.loads(msg.value.decode())
+            prediction = event["payload"]["after"]
+            credit_score = prediction["credit_score"]
+            prediction_id = prediction["prediction_id"]
+            counter += 1
+            print(f"Messages read: {counter}") 
+            
+            if credit_score > MIN_CREDIT_SCORE:
+                producer.send(
+                    topic=QUALIFIED_LEADS_TOPIC,
+                    value=json.dumps(prediction).encode("utf-8")
+                )
+                print(f"Sent Qualified Lead: {prediction_id=}, {credit_score=} to topic: {QUALIFIED_LEADS_TOPIC}")
 
 if __name__ == "__main__":
     read_from_debezium_topic()
